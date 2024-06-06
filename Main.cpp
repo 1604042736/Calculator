70fa615bf9563fd7024e5e28a5c1cca64880790d
YongjianWang <wangyongjian_501@hotmail.com>
2024-06-06 13:18:45 +0800

    --help选项

--- Main.cpp
+++ Main.cpp
@@ -15,6 +15,17 @@ namespace Win
 #include "Common.h"
 #include "Error.h"
 
+void print_help()
+{
+    printf("使用: Calculator [选项] [文件...]\n");
+    printf("选项:\n");
+    printf("--verbose\t输出过程信息\n");
+    printf("--shell\t打开shell(直接打开或者运行完文件后打开)\n");
+    printf("--help\t输出帮助\n");
+    printf("参数:\n");
+    printf("文件\t需要解析的一个或多个文件\n");
+}
+
 int main(int argc, char *argv[])
 {
 #ifdef _WIN32
@@ -35,6 +46,11 @@ int main(int argc, char *argv[])
                 verbose = true;
             else if (arg == "--shell")
                 open_shell = true;
+            else if (arg == "--help")
+            {
+                print_help();
+                return 0;
+            }
             else
                 filenames.push_back(arg);
         }
